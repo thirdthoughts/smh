6c6713ed480c4510ff81b69b43173a59e9a00293
jnewquist <83454561+jnewquist@users.noreply.github.com>
2024-06-09 14:47:44 -0400

    basic functionality complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,57 @@
-import Link from "next/link";
+"use client";
+import { useEffect, useState } from "react";
+import { filteredProducts } from "~/server/db/actions";
 
 export default function HomePage() {
+  const [searchParam, setSearchParam] = useState("");
+  const [results, setResults] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+  useEffect(() => {
+    filteredProducts(searchParam).then((res) => {
+      setResults(() => res);
+      setLoading(false);
+    });
+  }, [searchParam]);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      
+      <div>
+        Search:{" "}
+        <input
+          className="rounded-sm text-black"
+          type="text"
+          value={searchParam}
+          onChange={async (e) => {
+            setSearchParam(e.target.value);
+          }}
+        ></input>
+      </div>
+      { isLoading && <div>Loading...</div>}
+      {!!results.length && (
+        <table className="table-auto">
+          <thead>
+            <tr>
+              <th>Product Name</th>
+              <th>Product ID</th>
+              <th>Metro Area Title</th>
+              <th>Project Full Name</th>
+              <th>Project Group ID</th>
+            </tr>
+          </thead>
+          <tbody>
+            {results.map((res, index) => (
+              <tr key={index}>
+                <td>{res.productName || "NULL"}</td>
+                <td>{res.productID || "NULL"}</td>
+                <td>{res.metroAreaTitle || "NULL"}</td>
+                <td>{res.projectFullName || "NULL"}</td>
+                <td>{res.projectGroupID || "NULL"}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+      {!results.length && <div>No results found...</div>}
     </main>
   );
 }
--- src/server/db/actions.ts
+++ src/server/db/actions.ts
@@ -1,9 +1,9 @@
 "use server";
 import { db } from "~/server/db";
 import { Metro, Product, Project } from "./schema";
-import { eq, like, or } from "drizzle-orm/pg-core/expressions";
+import { eq, ilike, or } from "drizzle-orm/pg-core/expressions";
 
-export async function filteredProducts(searchParam: string) {
+export async function filteredProducts(searchParam: string) : Promise<any[]> {
   const likeParam = `%${searchParam}%`;
   const result = await db
     .select({
@@ -11,25 +11,28 @@ export async function filteredProducts(searchParam: string) {
       productID: Product.productID,
       metroAreaTitle: Metro.metroAreaTitle,
       projectFullName: Project.fullName,
-      projectGroupId: Project.projectGroupId,
+      projectGroupID: Project.projectGroupID,
     })
     .from(Product)
-    .fullJoin(Project, eq(Product.projectGroupID, Project.projectGroupId))
+    .fullJoin(Project, eq(Product.projectGroupID, Project.projectGroupID))
     .fullJoin(Metro, eq(Metro.metroAreaId, Project.metroAreaID))
     .where(
       or(
         /* despite the instructions I have chosen not to match against purely
          * numeric ids (primarykeys). This is easily changed if it is a requirement,
          * but I would prefer not to expose internal keys for security reasons.
+         * 
+         * additionally, I have assumed we do not wish to be case-sensitive in search.
+         * this is also an easy change if required.
          */
-        like(Metro.metroAreaStateAbr, likeParam),
-        like(Metro.MetroAreaStateName, likeParam),
-        like(Metro.metroAreaTitle, likeParam),
-        like(Project.fullName, likeParam),
-        like(Project.status, likeParam),
-        like(Product.projectName, likeParam),
-        like(Product.productID, likeParam),
-        like(Product.productName, likeParam)
+        ilike(Metro.metroAreaStateAbr, likeParam),
+        ilike(Metro.MetroAreaStateName, likeParam),
+        ilike(Metro.metroAreaTitle, likeParam),
+        ilike(Project.fullName, likeParam),
+        ilike(Project.status, likeParam),
+        ilike(Product.projectName, likeParam),
+        ilike(Product.productID, likeParam),
+        ilike(Product.productName, likeParam),
       ),
     );
 
